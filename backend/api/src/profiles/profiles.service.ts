104441fcacde740afc80c61dc8628b98a13071a6
Hector Gamiz <hectorgamiz@gmail.com>
2026-05-03 20:31:44 +0200

    Profiles services tests

--- backend/api/src/profiles/profiles.service.ts
+++ backend/api/src/profiles/profiles.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+    private profiles = [
+        {
+            id: randomUUID(),
+            name: 'Hector Gamiz',
+            description: 'Software developer.'
+        },
+        {
+            id: randomUUID(),
+            name: 'Martin Gamiz',
+            description: 'Estudiante de primaria'
+        },
+        {
+            id: randomUUID(),
+            name: 'Ana Moreno',
+            description: 'Estudiante de programación'
+        },
+    ];
+
+    findAll() {
+        return this.profiles;
+    }
+
+    findOne(id: string) {
+        return this.profiles.find((profile) => profile.id === id);
+    }
+}
