af55f7e5a230bfbe0baedd124f2a7fe9c527ade0
Hector <hgamiz-g@algo.com>
2026-05-19 10:27:05 +0200

    Database inititalization script for clean installations

---
+++ init-db.sql
@@ -0,0 +1,5 @@
+-- Initialize PostgreSQL permissions
+-- Database is already created by POSTGRES_DB environment variable
+
+-- Grant all privileges to bookstore user on bookstore database
+GRANT ALL PRIVILEGES ON DATABASE bookstore TO bookstore;
